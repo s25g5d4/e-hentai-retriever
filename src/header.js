67a8c76c7b43095eb647c54b9ceb34ae52e08c99
s25g5d4 <s25g5d4@gmail.com>
2016-09-06 12:42:33 +0800

    version 3.0.2

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@
 // @description e-hentai & exhentai image url retriever
 // @include     /^https?:\/\/g.e-hentai.org\/s\/.*/
 // @include     /^https?:\/\/exhentai.org\/s\/.*/
-// @version     3.0.1
+// @version     3.0.2
 // @grant       GM_xmlhttpRequest
 // @grant       unsafeWindow
 // ==/UserScript==
