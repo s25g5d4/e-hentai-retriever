120bf6e49ee13897106d94ef77cd5a32b4616d85
Zong Jhe Wu <s25g5d4@gmail.com>
2022-02-01 21:19:49 +0800

    Bump version to 4.2.0

--- src/header.js
+++ src/header.js
@@ -5,7 +5,7 @@ export default `
 // @description e-hentai & exhentai image url retriever
 // @include     /^https?:\/\/e-hentai.org\/s\/.*/
 // @include     /^https?:\/\/exhentai.org\/s\/.*/
-// @version     4.1.0
+// @version     4.2.0
 // @author      s25g5d4
 // @homepageURL https://github.com/s25g5d4/e-hentai-retriever
 // @grant       GM_xmlhttpRequest
