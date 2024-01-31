9a0a9f6668a1cdfa670382e7793f3d0304f7bccf
Zong Jhe Wu <s25g5d4@gmail.com>
2024-01-31 10:54:09 +0800

    bump version

--- build/ehr.user.js
+++ build/ehr.user.js
@@ -4,12 +4,13 @@
 // @description e-hentai & exhentai image url retriever
 // @include     /^https?://e-hentai.org/s/.*/
 // @include     /^https?://exhentai.org/s/.*/
-// @version     4.2.1
+// @version     4.3.0
 // @author      s25g5d4
 // @homepageURL https://github.com/s25g5d4/e-hentai-retriever
 // @grant       GM_xmlhttpRequest
 // @grant       unsafeWindow
 // ==/UserScript==
+
 (function () {
   'use strict';
 
@@ -705,7 +706,7 @@
           allPages.unshift(firstPage);
           return allPages
               .map(e => e.match(/<div[^>]*class="gdt\w"[^>]*>(?:(?:[^<]*)(?:<(?!\/div>)[^<]*)*)<\/div>/g))
-              .reduce((p, c) => p.concat(c)) // 2d array to 1d
+              .reduce((p, c) => p.concat(c), []) // 2d array to 1d
               .map(e => {
               const [, imgkey, page] = e.match(/s\/(\w+)\/\d+-(\d+)/);
               return { imgkey, page: parseInt(page, 10) };
